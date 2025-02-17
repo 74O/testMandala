a0d63a9d881ecd8ab7a2ea1fe6f9dd09a45793c9
74o <paskuini@gmail.com>
2025-02-17 14:29:47 +0100

    -m

--- testMandala.js
+++ testMandala.js
@@ -1,12 +1,37 @@
-function getMandalaData(){
+/**
+	* Muestra informacion del elemento seleccionado
+*/
+function informacion( e ){
+	let estilosComputados = window.getComputedStyle(e);
+	let estilos = e.style;
+	console.log( estilosComputados );
+	console.log( estilos );
+	let largo = e.getTotalLength();
+	let infoStr = e + e.id + " - " + largo;
+	console.log( infoStr );
+}
 
-   // Toma cada uno de los path y muestra su id y el total de su longitud en consola.
-   document.querySelectorAll('#weed_mandala_left path').forEach(camino=>{
-      let idElemento = camino.id;
-      let largo = camino.getTotalLength();
-      let displ = camino.style.display;
-      let stringInfo = idElemento + ' : ' + largo + ' : ' + displ
-      console.log(stringInfo);
-   });
+/**
+	* Animamos los elementos seleccionados
+*/
+function animate( e ){
+	let estilos = e.style;
+	estilos.opacity = .2;
+	estilos.fillOpacity = .3;
+}
 
+/**
+	* Selecciona los elementos 'path' del mandala seleccionado dado su id
+	* @param id representa el id del elemento cuyos path queremos animar
+	* @param speed representa la velocidad de carga en milisegundos
+*/
+function mandala( id, speed ){
+	let sel = '#' + id + ' path';
+	let items = document.querySelectorAll( sel );
+	for ( let i = 0; i < items.length; i++ ){
+		setTimeout( function(){
+			animate( items[i] );
+			//informacion( items[i] );
+		}, ( i + 1 ) * speed);
+	}
 }
